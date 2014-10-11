1fe08f234975349e11983069ccef244ba964c455
Davis Gao <naitnix@126.com>
2014-10-11 23:15:29 +0800

    modified

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-#redis_server = Rails.application.secrets.redis_server
-#redis_port = Rails.application.secrets.redis_port
-#redis_db_num = Rails.application.secrets.redis_db_num
-#redis_namespace = Rails.application.secrets.redis_namespace
-redis_server = 'localhost'
-redis_port   = 6379
-redis_db_num = 0
-redis_namespace = 'micro_crm_sidekiq'
+redis_server = Rails.application.secrets.redis_server
+redis_port = Rails.application.secrets.redis_port
+redis_db_num = Rails.application.secrets.redis_db_num
+redis_namespace = Rails.application.secrets.redis_namespace
+# redis_server = 'localhost'
+# redis_port   = 6379
+# redis_db_num = 0
+# redis_namespace = 'micro_crm_sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}", namespace: redis_namespace }
