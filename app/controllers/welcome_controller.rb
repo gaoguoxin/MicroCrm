1656dacc3b84586428c89c9d902414d012232860
Davis Gao <naitnix@126.com>
2014-10-31 18:41:42 +0800

    m

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def index 
-		UserMailer.publish_lesson('naitnix@126.com').deliver
+		#UserMailer.publish_lesson('naitnix@126.com').deliver
 	end
 	def tel
 	end
